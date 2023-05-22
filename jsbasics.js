40d3a395afadf9ff9c3ff7e3061f54be45228f37
kalyanihattimare <kalyani.kh14@gmail.com>
2023-05-22 21:57:55 +0530

    variables condition and fn

---
+++ jsbasics.js
@@ -0,0 +1,29 @@
+
+// console.log("Hello Kittu");
+
+// // there are 3 types of variables
+// var x=8
+// const y=4
+// let z=6
+// let a=3
+
+// // var global
+// // const and let block scope
+// // const can assing only one time 
+// // let multiple time assign
+
+// console.log(x,y,z)
+
+// // statements
+// if(z>4){
+//     console.log("Z is greater")
+// }
+// function
+
+// two way to declare fn
+function square(nums){
+   var double= nums*nums
+   console.log(double)
+}
+square(4)
+
